1f795769176cc0c5c3c91a65ddb574513a47ee12
Plínio Alcântara <plinio.alcantara@dcx.ufpb.br>
2025-10-15 17:30:34 -0300

    Adicionando as fotos do carrosel

--- site-connect-fibra/src/app/carousel/carousel.component.ts
+++ site-connect-fibra/src/app/carousel/carousel.component.ts
@@ -7,17 +7,18 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 })
 export class CarouselComponent implements OnInit, OnDestroy {
   images: string[] = [
-    'assets/banner1.jpg',
-    'assets/banner2.jpg',
-    'assets/banner3.jpg'
+    'assets/images/banner1.jpg',   // você sempre conectado
+    'assets/images/banner2.jpg',   // amigo indica amigo
+    'assets/images/banner3.jpg'    // pais conectados
   ];
+
   currentIndex = 0;
   private intervalId: any;
 
   ngOnInit() {
     this.intervalId = setInterval(() => {
       this.currentIndex = (this.currentIndex + 1) % this.images.length;
-    }, 5000); // muda a cada 5 segundos
+    }, 5000); // troca a cada 5 segundos
   }
 
   ngOnDestroy() {
