202400109828b4465ce7ddcf8ca462338447643c
Plínio Alcântara <plinio.alcantara@dcx.ufpb.br>
2025-10-16 13:57:24 -0300

    Aplicando estilização no component de planos

--- site-connect-fibra/src/app/plans/plans.component.ts
+++ site-connect-fibra/src/app/plans/plans.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, QueryList, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-plans',
   templateUrl: './plans.component.html',
-  styleUrl: './plans.component.css'
+  styleUrls: ['./plans.component.css']
 })
-export class PlansComponent {
+export class PlansComponent implements AfterViewInit {
+  @ViewChildren('planCard') planCards!: QueryList<ElementRef>;
 
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach(entry => {
+          const element = entry.target as HTMLElement;
+          if (entry.isIntersecting) {
+            element.classList.add('animate');
+          } else {
+            // Quando sai da tela, remove para permitir repetir
+            element.classList.remove('animate');
+          }
+        });
+      },
+      { threshold: 0.3 } // ativa quando 30% do card estiver visível
+    );
+
+    this.planCards.forEach(card => observer.observe(card.nativeElement));
+  }
 }
