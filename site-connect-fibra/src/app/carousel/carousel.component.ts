dded06635544261d05e13c361bfd4ebc8d92d77d
Plínio Alcântara <plinio.alcantara@dcx.ufpb.br>
2025-11-05 14:43:44 -0300

    Alterando as imagens promocionais e adicionando o link do wpp

--- site-connect-fibra/src/app/carousel/carousel.component.ts
+++ site-connect-fibra/src/app/carousel/carousel.component.ts
@@ -3,18 +3,35 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  styleUrls: ['./carousel.component.css'],
 })
 export class CarouselComponent implements OnInit, OnDestroy {
-
-  images: { src: string; position: string }[] = [
-    { src: 'assets/images/sorteio.jpg', position: 'top center' }, 
-    { src: 'assets/images/banner1.1.jpg', position: 'top center' }, 
-    { src: 'assets/images/banner2.jpg', position: 'top center' }   
+  images: { src: string; position: string; message: string }[] = [
+    {
+      src: 'assets/images/banners/black.jpg',
+      position: 'top center',
+      message: 'Olá! Vi a promoção da Black Friday e quero saber mais sobre as ofertas!'
+    },
+    {
+      src: 'assets/images/banners/sorteio.jpg',
+      position: 'top center',
+      message: 'Olá! Gostaria de participar do sorteio da Connect Fibra!'
+    },
+    {
+      src: 'assets/images/banners/banner1.1.jpg',
+      position: 'top center',
+      message: 'Oi! Tenho interesse na promoção do plano 600 Mega!'
+    },
+    {
+      src: 'assets/images/banners/banner_indicação.jpg',
+      position: 'top center',
+      message: 'Olá! Quero saber mais sobre o programa "Amigo Indica Amigo"!'
+    },
   ];
 
   currentIndex = 0;
   private intervalId: any;
+  private whatsappNumber = '5583991616542';
 
   ngOnInit() {
     this.intervalId = setInterval(() => {
@@ -24,5 +41,10 @@ export class CarouselComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     clearInterval(this.intervalId);
+  }
+
+  abrirWhatsApp(message: string) {
+    const url = `https://wa.me/${this.whatsappNumber}?text=${encodeURIComponent(message)}`;
+    window.open(url, '_blank');
   }
 }
